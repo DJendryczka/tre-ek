919fa664bb48e3622bb67e3506a9cce97f525808
Dariusz Jendryczka <dariusz.jendryczka@gmail.com>
2023-12-04 19:37:47 +0100

    Added carousel

--- script.js
+++ script.js
@@ -36,3 +36,49 @@ const navSlide = () => {
 }
 
 navSlide();
+
+/* Carousel */
+const slides = document.getElementsByClassName('carousel-item');
+let slidePosition = 0;
+const totalSlides = slides.length;
+const timeOut = setTimeout('autoSlide', 1000)
+console.log(slides);
+
+
+
+function hideAllSlides() {
+  for (let slide of slides) {
+    slide.classList.remove('carousel-item-visible');
+    slide.classList.add('carousel-item-hidden');
+  }
+}
+ function autoSlide(){
+    hideAllSlides()
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
+  slides[slidePosition].classList.add('carousel-item-visible');
+  setTimeout(autoSlide, 5000);
+ }
+ 
+// function moveToNextSlide() {
+//     hideAllSlides()
+//   if (slidePosition === totalSlides - 1) {
+//     slidePosition = 0;
+//   } else {
+//     slidePosition++;
+//   }
+//   slides[slidePosition].classList.add('carousel-item-visible');
+// }
+// function moveToPrevSlide() {
+//     hideAllSlides()
+//     if (slidePosition === 0) {
+//         slidePosition = totalSlides - 1;
+//     } else {
+//         slidePosition--;
+//     }
+//   slides[slidePosition].classList.add('carousel-item-visible');
+// }
+window.onload=autoSlide()
